2e3a033bda8231993426f771bcccca2e3b81db3e
Chris <chris@landlordwebsolutions.com>
2019-07-25 15:07:59 -0400

    Only souce aliases in the .bashrc if it's not already present

--- apply.sh
+++ apply.sh
@@ -2,7 +2,6 @@
 
 # apply environment
 
-## TODO: check if already written before appending again OR 
-## version rc file completely
-# add aliases to user's rc
-echo "source ~/env/aliases" >> ~/.bashrc
+if ! grep -q "~/env/aliases" ~/.bashrc; then
+    echo "source ~/env/aliases" >> ~/.bashrc
+fi
