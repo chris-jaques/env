9c5eb86bebcb32df35f3f5cc55e448a6e76fc98c
chris-jaques <chris@landlordwebsolutions.com>
2019-07-28 11:11:14 -0400

    Add a local.al file for computer-specific directory aliases

--- install.sh
+++ install.sh
@@ -4,5 +4,13 @@
 cd ~/
 git clone https://github.com/chris-jaques/env.git
 
+# create a local alias file
+if ! [ -f ~/env/local.al ]; then
+    echo "#!/bin/bash\n#\n# Local Env\n# for directory aliases specific to this computer\n#\n#\n\n" >> ~/env/local.al
+fi
+
 # apply environment
 sh ~/env/apply.sh
+
+# load the env
+exec bash
