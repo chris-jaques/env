a4a70ac853e66b57977075522d138aea82547cc1
Chris <chris@landlordwebsolutions.com>
2019-08-05 11:24:56 -0400

    add 'e' alias to local.al definition for cda command to work

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ git clone https://github.com/chris-jaques/env.git
 
 # create a local alias file
 if ! [ -f ~/env/local.al ]; then
-    echo "#!/bin/sh\n#\n# Local Env\n# for directory aliases specific to this computer\n#\n#" > ~/env/local.al
+    echo "#!/bin/sh\n#\n# Local Env\n# for directory aliases specific to this computer\n#\n#\n# Env\nalias e='cd ~/env'" > ~/env/local.al
 fi
 
 # apply environment
@@ -17,6 +17,5 @@ fi
 # load the env
 source ~/.bashrc
 
-# Ensure local cd helpers exist
-cda ~/env e Env
+# Add cd alias for dev directory
 cda ~/dev dev "Root dir for dev projects"
