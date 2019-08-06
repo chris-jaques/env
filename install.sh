5e08fcbd878d9389cc4fdbae4fa3e6a26f6f4a5f
chris-jaques <chris@landlordwebsolutions.com>
2019-08-06 12:16:28 -0400

    don't bother loading the env from install.sh, since it doesn't work anyway  - create ~/dev if it doesn't exist

--- install.sh
+++ install.sh
@@ -7,13 +7,10 @@ git clone https://github.com/chris-jaques/env.git
 # create a local alias file
 if ! [ -f ~/env/local.al ]; then
     echo "#!/bin/sh\n#\n# Local Env\n# for directory aliases specific to this computer\n#\n#\n# Env\nalias e='cd ~/env'\n\n# Root dir for dev projects\nalias dev='cd ~/dev'" > ~/env/local.al
+    mkdir -p ~/dev
 fi
 
 # apply environment
 if ! grep -q "~/env/loadEnv" ~/.bashrc; then
     echo "source ~/env/loadEnv" >> ~/.bashrc
 fi
-
-# load the env
-. ~/env/loadEnv
-
